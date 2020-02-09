3ab883474f37a6305d4bcfeca5e54fd789b8e990
Erin Millard <ezzatron@gmail.com>
2020-02-10 03:03:28 +1000

    Debounce animation correctly

--- main.js
+++ main.js
@@ -21,40 +21,59 @@ function draw (canvas, ctx) {
   ctx.fillRect(doublePadding, doublePadding, width - quadPadding, height - quadPadding)
 }
 
-const RESIZE_TTL = 200
-const throttledResizeCanvas = throttle(RESIZE_TTL, resizeCanvas)
+function createResizeMonitor (viewport, canvas) {
+  let animation, timeoutId
+  let nextWidth = canvas.width
+  let nextHeight = canvas.height
 
-function monitorViewport (viewport, canvas) {
-  const {clientWidth: width, clientHeight: height} = viewport
+  const animateResize = debounce(400, (width, height, scaleX, scaleY) => {
+    if (animation) return
 
-  if (canvas.width !== width || canvas.height !== height) throttledResizeCanvas(width, height, canvas)
-}
+    canvas.width = width
+    canvas.height = height
 
-function resizeCanvas (width, height, canvas) {
-  const deltaWidth = canvas.width / width
-  const deltaHeight = canvas.height / height
-
-  canvas.width = width
-  canvas.height = height
-
-  canvas.animate(
-    [
+    animation = canvas.animate(
+      [
+        {transform: `scale(${scaleX}, ${scaleY})`},
+        {transform: 'none'},
+      ],
       {
-        transform: `scale(${deltaWidth}, ${deltaHeight})`,
+        duration: 200,
+        easing: 'ease-in-out',
       },
-      {
-        transform: 'none',
-      },
-    ],
-    {
-      duration: RESIZE_TTL,
-      easing: 'ease-in-out',
-    },
-  )
+    )
+    animation.addEventListener('finish', () => { animation = null }, {once: true})
+  })
+
+  return function monitorResize () {
+    const {clientWidth: width, clientHeight: height} = viewport
+
+    if (canvas.width === width && canvas.height === height) return
+
+    const deltaWidth = canvas.width / width
+    const deltaHeight = canvas.height / height
+    const isWidthIncreasing = deltaWidth < 1
+    const isHeightIncreasing = deltaHeight < 1
+    const needsAnimation = isWidthIncreasing || isHeightIncreasing
+
+    if (!isWidthIncreasing) canvas.width = width
+    if (!isHeightIncreasing) canvas.height = height
+
+    if (!needsAnimation) return
+    if (nextWidth === width && nextHeight === height) return
+
+    nextWidth = width
+    nextHeight = height
+    const scaleX = deltaWidth > 1 ? 1 : deltaWidth
+    const scaleY = deltaHeight > 1 ? 1 : deltaHeight
+
+    animateResize(width, height, scaleX, scaleY)
+  }
 }
 
 function createLoop (viewport, canvas, ctx) {
   const state = {}
+  const monitorResize = createResizeMonitor(viewport, canvas)
   let previousTimestamp = -1
 
   return loop
@@ -62,7 +81,7 @@ function createLoop (viewport, canvas, ctx) {
   function loop (timestamp) {
     const elapsed = previousTimestamp > 0 ? timestamp - previousTimestamp : 0
 
-    monitorViewport(viewport, canvas)
+    monitorResize()
     update(state, elapsed)
     draw(canvas, ctx)
 
@@ -72,27 +91,31 @@ function createLoop (viewport, canvas, ctx) {
 }
 
 function main () {
+  const viewport = document.documentElement
+  const {clientWidth: width, clientHeight: height} = viewport
+
   const canvas = document.createElement('canvas')
+  canvas.width = width
+  canvas.height = height
   canvas.style.display = 'block'
   canvas.style.willChange = 'transform'
 
   const ctx = canvas.getContext('2d')
-  const loop = createLoop(document.documentElement, canvas, ctx)
+  const loop = createLoop(viewport, canvas, ctx)
 
   document.body.appendChild(canvas)
   window.requestAnimationFrame(loop)
 }
 
-function throttle (delay, fn) {
-  let trailId, waitId
+function debounce (delay, fn) {
+  let timeoutId
 
   return (...args) => {
-    if (trailId) clearTimeout(trailId)
-    trailId = setTimeout(() => { fn(...args) }, delay)
+    if (timeoutId) clearTimeout(timeoutId)
 
-    if (waitId) return
-
-    fn(...args)
-    waitId = setTimeout(() => { waitId = null }, delay)
+    timeoutId = setTimeout(() => {
+      fn(...args)
+      timeoutId = null
+    }, delay)
   }
 }
